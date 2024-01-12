2fc8fe4eda9bf11bbddd8739d6e065cbffe40933
David Škudar <dskudar20@foi.hr>
2024-01-12 21:10:23 +0100

    Preuređena forma prijave

--- Software/Scriptify/Scriptify/frmRegister.Designer.cs
+++ Software/Scriptify/Scriptify/frmRegister.Designer.cs
@@ -64,7 +64,7 @@ namespace Scriptify
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.Purple;
-            this.label1.Location = new System.Drawing.Point(103, 31);
+            this.label1.Location = new System.Drawing.Point(103, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 30);
             this.label1.TabIndex = 0;
