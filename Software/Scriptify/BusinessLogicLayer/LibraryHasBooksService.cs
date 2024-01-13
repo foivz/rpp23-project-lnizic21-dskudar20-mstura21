17177203a60729fc394f5b2689aa77bd1bfc3b0c
David Škudar <dskudar20@foi.hr>
2024-01-13 20:21:45 +0100

    Kreiran repository za dohvacanje knjiga neke knjiznice.

---
+++ Software/Scriptify/BusinessLogicLayer/LibraryHasBooksService.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class LibraryHasBooksService
+    {
+
+        public List<Library_has_Books> GetAllGetAllBooksFromLibrary(int idLibrary)
+        {
+            using (var repo = new LibraryHasBooksRepository())
+            {
+                return repo.GetAllBooksFromLibrary(idLibrary).ToList();
+            }
+        }
+    }
+}
---
+++ Software/Scriptify/DataAccessLayer/Repositories/LibraryHasBooksRepository.cs
@@ -0,0 +1,38 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class LibraryHasBooksRepository : IDisposable
+    {
+        protected EntityModels Context { get; set; }
+        public DbSet<Library_has_Books> Entities { get; set; }
+        public LibraryHasBooksRepository()
+        {
+            Context = new EntityModels();
+            Entities = Context.Set<Library_has_Books>();
+        }
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public IQueryable<Library_has_Books> GetAllBooksFromLibrary(int idLibrary)
+        {
+            var sql = from e in Entities
+                      where e.Library_idLibrary == idLibrary
+                      select e;
+            return sql;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
