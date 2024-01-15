c151e1cec05442b27011c51d068a90fc553bed6f
David Škudar <dskudar20@foi.hr>
2024-01-15 15:02:46 +0100

    Kreiran repository za upravljanje posudbama i sankcijama.

---
+++ Software/Scriptify/DataAccessLayer/Repositories/LoanHistoryAndSanctionRepository.cs
@@ -0,0 +1,40 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class LoanHistoryAndSanctionRepository : IDisposable
+    {
+        protected EntityModels Context { get; set; }
+        public DbSet<Loan> Entities { get; set; }
+        public LoanHistoryAndSanctionRepository()
+        {
+            Context = new EntityModels();
+            Entities = Context.Set<Loan>();
+        }
+
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public IQueryable<Loan> GetAllExpiredLoans()
+        {
+            DateTime today = DateTime.Now.Date;
+
+            var sql = from e in Entities
+                      where e.planned_return <= today
+                      select e;
+            return sql;
+        }
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
