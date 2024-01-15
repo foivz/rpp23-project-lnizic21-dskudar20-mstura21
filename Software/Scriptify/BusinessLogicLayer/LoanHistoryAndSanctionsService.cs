f62e95fd0b363d8c7fbc95b291505e2546f7a563
David Škudar <dskudar20@foi.hr>
2024-01-15 15:04:32 +0100

    Kreiran servis u BusinessLogicLayeru za upravljanje posudbama i sankcijama.

---
+++ Software/Scriptify/BusinessLogicLayer/LoanHistoryAndSanctionsService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class LoanHistoryAndSanctionsService
+    {
+        public List<Loan> GetAllExpiredLoans()
+        {
+            using (var repo = new LoanHistoryAndSanctionRepository())
+            {
+                return repo.GetAllExpiredLoans().ToList();
+            }
+        }
+    }
+}
