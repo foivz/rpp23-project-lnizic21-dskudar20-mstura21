443c0f1ddf09d301454fa71025df95cd6ea8dd20
David Škudar <dskudar20@foi.hr>
2024-01-16 13:59:23 +0100

    Kreiran repository za izvještaje i statistiku.

---
+++ Software/Scriptify/DataAccessLayer/Repositories/ReportsAndStatisticsRepository.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ReportsAndStatisticsRepository : IDisposable
+    {
+        protected EntityModels Context { get; set; }
+        public DbSet<Book> Entities { get; set; }
+        public ReportsAndStatisticsRepository()
+        {
+            Context = new EntityModels();
+            Entities = Context.Set<Book>();
+        }
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
