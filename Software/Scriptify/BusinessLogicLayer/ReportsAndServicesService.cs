e9e517070c3af14b8e369c7bd7133921bced7cf3
David Škudar <dskudar20@foi.hr>
2024-01-16 15:57:35 +0100

    Kreiran servis za izvještaje i statistiku.

---
+++ Software/Scriptify/BusinessLogicLayer/ReportsAndServicesService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class ReportsAndServicesService
+    {
+        public List<Book> GetTopBooks(int librarianId)
+        {
+            using (var repo = new ReportsAndStatisticsRepository())
+            {
+                return repo.GetTopBooks(librarianId).ToList();
+            }
+        }
+    }
+}
--- Software/Scriptify/DataAccessLayer/Repositories/ReportsAndStatisticsRepository.cs
+++ Software/Scriptify/DataAccessLayer/Repositories/ReportsAndStatisticsRepository.cs
@@ -22,7 +22,29 @@ namespace DataAccessLayer.Repositories
             return Context.SaveChanges();
         }
 
+        public IQueryable<Book> GetTopBooks(int librarianId)
+        {
 
+            var sql = (from loan in Entities
+                       join book in Context.Books on loan.idBook equals book.idBook
+                       join libraryBook in Context.Library_has_Books on book.idBook equals libraryBook.Books_idBook
+                       join library in Context.Librarians on libraryBook.Library_idLibrary equals library.Library_idLibrary
+                       where library.idLibrarians == librarianId
+                       group loan by new { book.idBook, book.book_name, book.overview, book.author, book.genre } into g
+                       select new Book
+                       {
+
+                           idBook = g.Key.idBook,
+                           book_name = g.Key.book_name,
+                           overview = g.Key.overview,
+                           author = g.Key.author,
+                           genre = g.Key.genre,
+
+                       })
+        .Take(10);
+
+            return sql;
+        }
         public void Dispose()
         {
             throw new NotImplementedException();
