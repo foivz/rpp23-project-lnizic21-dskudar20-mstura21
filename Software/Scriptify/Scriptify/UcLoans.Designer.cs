83ddf9ce24603f760a1f5819f3a15e4b4dfd0a1a
mustr <mustr@MARINO>
2024-01-26 00:24:56 +0100

    Napravljena forma za posudbe u tijeku.

--- Software/Scriptify/Scriptify/UcLoans.Designer.cs
+++ Software/Scriptify/Scriptify/UcLoans.Designer.cs
@@ -29,26 +29,56 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dgvStatistics = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatistics)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(353, 9);
+            this.label1.Location = new System.Drawing.Point(265, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 29);
+            this.label1.Size = new System.Drawing.Size(71, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Loans";
             // 
+            // dgvStatistics
+            // 
+            this.dgvStatistics.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStatistics.Location = new System.Drawing.Point(12, 60);
+            this.dgvStatistics.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvStatistics.Name = "dgvStatistics";
+            this.dgvStatistics.RowHeadersWidth = 51;
+            this.dgvStatistics.RowTemplate.Height = 24;
+            this.dgvStatistics.Size = new System.Drawing.Size(516, 231);
+            this.dgvStatistics.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(20, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pending loans";
+            // 
             // UcLoans
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvStatistics);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UcLoans";
-            this.Size = new System.Drawing.Size(806, 525);
+            this.Size = new System.Drawing.Size(604, 427);
             this.Load += new System.EventHandler(this.UcLoans_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStatistics)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvStatistics;
+        private System.Windows.Forms.Label label2;
     }
 }
