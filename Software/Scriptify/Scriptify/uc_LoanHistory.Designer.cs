15e0e67a4a4f8fccbca3d7e74e38efa2558a4bb3
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-25 23:10:40 +0100

    Kreiran uc za evidenciju

---
+++ Software/Scriptify/Scriptify/uc_LoanHistory.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Scriptify
+{
+    partial class uc_LoanHistory
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLoanHistory = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEvidentiraj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoanHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLoanHistory
+            // 
+            this.dgvLoanHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLoanHistory.Location = new System.Drawing.Point(0, 79);
+            this.dgvLoanHistory.Name = "dgvLoanHistory";
+            this.dgvLoanHistory.RowHeadersWidth = 51;
+            this.dgvLoanHistory.RowTemplate.Height = 24;
+            this.dgvLoanHistory.Size = new System.Drawing.Size(809, 266);
+            this.dgvLoanHistory.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Evidentiraj povratak knjige";
+            // 
+            // btnEvidentiraj
+            // 
+            this.btnEvidentiraj.Location = new System.Drawing.Point(360, 382);
+            this.btnEvidentiraj.Name = "btnEvidentiraj";
+            this.btnEvidentiraj.Size = new System.Drawing.Size(108, 34);
+            this.btnEvidentiraj.TabIndex = 2;
+            this.btnEvidentiraj.Text = "Evidentiraj";
+            this.btnEvidentiraj.UseVisualStyleBackColor = true;
+            this.btnEvidentiraj.Click += new System.EventHandler(this.btnEvidentiraj_Click);
+            // 
+            // uc_LoanHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnEvidentiraj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvLoanHistory);
+            this.Name = "uc_LoanHistory";
+            this.Size = new System.Drawing.Size(809, 445);
+            this.Load += new System.EventHandler(this.uc_LoanHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoanHistory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLoanHistory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEvidentiraj;
+    }
+}
---
+++ Software/Scriptify/Scriptify/uc_LoanHistory.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogicLayer;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scriptify
+{
+    public partial class uc_LoanHistory : UserControl
+    {
+        private LoanHistoryAndSanctionsService loanHistoryAndSanctionsService;
+        private Librarian user = new Librarian();
+        public uc_LoanHistory(Librarian user)
+        {
+            InitializeComponent();
+            this.user = user;
+            loanHistoryAndSanctionsService = new LoanHistoryAndSanctionsService();
+        }
+
+        private void btnEvidentiraj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uc_LoanHistory_Load(object sender, EventArgs e)
+        {
+            ShowLoans();
+        }
+        private void ShowLoans()
+        {
+            List<Loan> loans = loanHistoryAndSanctionsService.GetAllExpiredLoans(user.idLibrarians);
+            dgv = loans;
+            dgvLoanHistoryAndSanctions.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+    }
+}
