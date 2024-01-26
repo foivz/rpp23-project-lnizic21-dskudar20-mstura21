a500c05cd82fa3c5b243a86e18b51c4615ab904c
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-26 20:00:54 +0100

    Dodan i implementiran service za Rezervaicje.

---
+++ Software/Scriptify/BusinessLogicLayer/ReservationService.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class ReservationService
+    {
+
+        public List<Reservation_Projection> GetReservations(int librarianId)
+        {
+            using (var repo = new ReservationsRepository())
+            {
+                return repo.GetReservations(librarianId).ToList();
+            }
+        }
+
+        public bool AcceptReservation(Reservation_Projection reservation)
+        {
+            bool isAccepted = false;
+            using (var repo = new ReservationsRepository())
+            {
+                int affectedRow = repo.ReservationAccepted(reservation);
+                isAccepted = affectedRow > 0;
+            }
+            return isAccepted;
+        }
+
+    }
+}
