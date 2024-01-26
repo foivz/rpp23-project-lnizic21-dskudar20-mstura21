365d58832a4be0267225f947fcf6c90366f6a6ef
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-26 21:13:46 +0100

    Grafički dizajn za user controlu rezervacija

--- Software/Scriptify/Scriptify/uc_Reservation.Designer.cs
+++ Software/Scriptify/Scriptify/uc_Reservation.Designer.cs
@@ -28,10 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.dgvReservation = new System.Windows.Forms.DataGridView();
+            this.btnAccept = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReservation
+            // 
+            this.dgvReservation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservation.Location = new System.Drawing.Point(93, 60);
+            this.dgvReservation.Name = "dgvReservation";
+            this.dgvReservation.RowHeadersWidth = 51;
+            this.dgvReservation.RowTemplate.Height = 24;
+            this.dgvReservation.Size = new System.Drawing.Size(712, 366);
+            this.dgvReservation.TabIndex = 0;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(390, 450);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 1;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // uc_Reservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.dgvReservation);
+            this.Name = "uc_Reservation";
+            this.Size = new System.Drawing.Size(898, 516);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvReservation;
+        private System.Windows.Forms.Button btnAccept;
     }
 }
