770829409d381c23fb06510a9490d760985e3d1b
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-26 12:55:53 +0100

    Dodano dohvaćanje lista posudbi iz baze koje su još u trajanju te metoda za promjenu statusa posudbe u završeno.

--- Software/Scriptify/DataAccessLayer/Repositories/LoanHistoryAndSanctionRepository.cs
+++ Software/Scriptify/DataAccessLayer/Repositories/LoanHistoryAndSanctionRepository.cs
@@ -10,7 +10,7 @@ namespace DataAccessLayer.Repositories
 {
     public class LoanHistoryAndSanctionRepository : IDisposable
     {
-        protected EntityModels Context { get; set; }
+        public EntityModels Context { get; set; }
         public DbSet<Loan> Entities { get; set; }
         public LoanHistoryAndSanctionRepository()
         {
@@ -35,6 +35,37 @@ namespace DataAccessLayer.Repositories
 
             return loans;
         }
+
+        public IQueryable<Loan> GetLoansInProgress(int librarianId)
+        {
+
+            var loans = from loan in Entities
+                        join book in Context.Library_has_Books on loan.book_id equals book.Books_idBook
+                        join library in Context.Librarians on book.Library_idLibrary equals library.Library_idLibrary
+                        where library.idLibrarians == librarianId && loan.loan_status == "In progress"
+                        select loan;
+
+            return loans;
+        }
+
+
+        public int LoanStatusCompleted(Loan loan, bool saveChanges = true)
+        {
+            var existingLoan = Entities.FirstOrDefault(e => e.idUser == loan.idUser && e.book_id == loan.book_id);
+
+            if (existingLoan != null)
+            {
+                existingLoan.loan_status = "Completed";
+
+                if (saveChanges)
+                {
+                    return SaveChanges();
+                }
+            }
+
+            return 0;
+        }
+
 
         public int IssueSanction(Loan loan, bool saveChanges = true)
         {
