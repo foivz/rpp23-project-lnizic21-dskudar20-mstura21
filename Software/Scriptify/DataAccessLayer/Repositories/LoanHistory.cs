c37fcec03c65f89151c767dd4695780fe36d0b1b
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-26 20:49:21 +0100

    Kreirana implementacija za dohvat posudbi te promjene statusa u izvršeno.

--- Software/Scriptify/DataAccessLayer/Repositories/LoanHistory.cs
+++ Software/Scriptify/DataAccessLayer/Repositories/LoanHistory.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using EntityLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class LoanHistory 
+    public class LoanHistory :IDisposable
+    {
+        public EntityModels Context { get; set; }
+        public DbSet<Loan> Entities { get; set; }
+
+        public LoanHistory()
+        {
+            Context = new EntityModels();
+            Entities = Context.Set<Loan>();
+        }
+
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public IQueryable<Loan> GetLoansInProgress(int librarianId)
+        {
+
+            var loans = from loan in Entities
+                        join book in Context.Library_has_Books on loan.book_id equals book.Books_idBook
+                        join library in Context.Librarians on book.Library_idLibrary equals library.Library_idLibrary
+                        where library.idLibrarians == librarianId && loan.loan_status == "In progress"
+                        select loan;
+
+            return loans;
+        }
+
+        public int LoanStatusCompleted(Loan loan, bool saveChanges = true)
+        {
+            var existingLoan = Entities.FirstOrDefault(e => e.idUser == loan.idUser && e.book_id == loan.book_id);
+
+            if (existingLoan != null)
+            {
+                existingLoan.loan_status = "Completed";
+
+                if (saveChanges)
+                {
+                    return SaveChanges();
+                }
+            }
+
+            return 0;
+        }
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
     
 }
