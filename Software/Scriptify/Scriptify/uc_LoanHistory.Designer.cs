f32871bd1685fdd7f59c0cfe6952d4a95b64093c
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-28 13:25:54 +0100

    Updejtan UI contorla za evidenciju i rezervaciju

--- Software/Scriptify/Scriptify/uc_LoanHistory.Designer.cs
+++ Software/Scriptify/Scriptify/uc_LoanHistory.Designer.cs
@@ -30,23 +30,24 @@
         {
             this.dgvLoanHistory = new System.Windows.Forms.DataGridView();
             this.btnEvidentiraj = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLoanHistory)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvLoanHistory
             // 
             this.dgvLoanHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLoanHistory.Location = new System.Drawing.Point(3, 2);
+            this.dgvLoanHistory.Location = new System.Drawing.Point(3, 43);
             this.dgvLoanHistory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvLoanHistory.Name = "dgvLoanHistory";
             this.dgvLoanHistory.RowHeadersWidth = 51;
             this.dgvLoanHistory.RowTemplate.Height = 24;
-            this.dgvLoanHistory.Size = new System.Drawing.Size(620, 281);
+            this.dgvLoanHistory.Size = new System.Drawing.Size(852, 282);
             this.dgvLoanHistory.TabIndex = 0;
             // 
             // btnEvidentiraj
             // 
-            this.btnEvidentiraj.Location = new System.Drawing.Point(513, 288);
+            this.btnEvidentiraj.Location = new System.Drawing.Point(516, 343);
             this.btnEvidentiraj.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnEvidentiraj.Name = "btnEvidentiraj";
             this.btnEvidentiraj.Size = new System.Drawing.Size(109, 23);
@@ -55,10 +56,21 @@
             this.btnEvidentiraj.UseVisualStyleBackColor = true;
             this.btnEvidentiraj.Click += new System.EventHandler(this.btnEvidentiraj_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(354, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Loans In Progress";
+            // 
             // uc_LoanHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEvidentiraj);
             this.Controls.Add(this.dgvLoanHistory);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -67,6 +79,7 @@
             this.Load += new System.EventHandler(this.uc_LoanHistory_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLoanHistory)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +87,6 @@
 
         private System.Windows.Forms.DataGridView dgvLoanHistory;
         private System.Windows.Forms.Button btnEvidentiraj;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Software/Scriptify/Scriptify/uc_Reservation.Designer.cs
+++ Software/Scriptify/Scriptify/uc_Reservation.Designer.cs
@@ -30,43 +30,56 @@
         {
             this.dgvReservation = new System.Windows.Forms.DataGridView();
             this.btnAccept = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvReservation
             // 
             this.dgvReservation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReservation.Location = new System.Drawing.Point(2, 2);
-            this.dgvReservation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvReservation.Location = new System.Drawing.Point(3, 58);
+            this.dgvReservation.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvReservation.Name = "dgvReservation";
             this.dgvReservation.RowHeadersWidth = 51;
             this.dgvReservation.RowTemplate.Height = 24;
-            this.dgvReservation.Size = new System.Drawing.Size(449, 180);
+            this.dgvReservation.Size = new System.Drawing.Size(868, 222);
             this.dgvReservation.TabIndex = 0;
             // 
             // btnAccept
             // 
-            this.btnAccept.Location = new System.Drawing.Point(395, 186);
-            this.btnAccept.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAccept.Location = new System.Drawing.Point(644, 293);
+            this.btnAccept.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(56, 19);
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
             this.btnAccept.TabIndex = 1;
             this.btnAccept.Text = "Accept";
             this.btnAccept.UseVisualStyleBackColor = true;
             this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(345, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Reservations";
+            // 
             // uc_Reservation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAccept);
             this.Controls.Add(this.dgvReservation);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "uc_Reservation";
-            this.Size = new System.Drawing.Size(674, 419);
+            this.Size = new System.Drawing.Size(899, 516);
             this.Load += new System.EventHandler(this.uc_Reservation_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.dgvReservation)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +87,6 @@
 
         private System.Windows.Forms.DataGridView dgvReservation;
         private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Label label1;
     }
 }
