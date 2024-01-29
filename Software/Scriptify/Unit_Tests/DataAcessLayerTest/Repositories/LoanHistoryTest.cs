9be8ed17205907805230668e1492c12872ce4f30
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-29 18:38:16 +0100

    Napravljeni testovi za evidenciju i  rezervacije.

---
+++ Software/Scriptify/Unit_Tests/DataAcessLayerTest/Repositories/LoanHistoryTest.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Unit_Tests.DataAcessLayerTest.Repositories
+{
+    [TestClass]
+    public class LoanHistoryTest
+    {
+        [TestMethod]
+        public void GetLoansInProgress()
+        {
+            using (var repo = new LoanHistoryRepository())
+            {
+                var loans = repo.GetLoansInProgress(4);
+                Assert.IsNotNull(loans);
+            }
+        }
+       
+    }
+}
+
---
+++ Software/Scriptify/Unit_Tests/DataAcessLayerTest/Repositories/ReservationTest.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests.DataAcessLayerTest.Repositories
+{
+    [TestClass]
+    public class ReservationTest
+    {
+
+
+        [TestMethod]
+        public void GetLoansInProgress()
+        {
+            using (var repo = new ReservationsRepository())
+            {
+                var reservations = repo.GetReservations(4);
+                Assert.IsNotNull(reservations);
+            }
+        }
+    }
+}
