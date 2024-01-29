ab425d89ef7cb734a885d9b4b42b469d446381c1
David Škudar <dskudar20@foi.hr>
2024-01-29 15:33:09 +0100

    Kreirani testovi za LoanHistoryAndSanctionRepository.

--- Software/Scriptify/Scriptify/uc_LoanHistoryAndSanctions.cs
+++ Software/Scriptify/Scriptify/uc_LoanHistoryAndSanctions.cs
@@ -76,5 +76,7 @@ namespace Scriptify {
                 }
             }
         }
+
+ 
     }
 }
---
+++ Software/Scriptify/Unit_Tests/DataAcessLayerTest/Repositories/LoanHistoryAndSanctionRepositoryTest.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Tests.DataAcessLayerTest.Repositories
+{
+    [TestClass]
+    public class LoanHistoryAndSanctionRepositoryTest
+    {
+
+        [TestMethod]
+        public void GetAllExpiredLoans()
+        {
+            using (var repo = new LoanHistoryAndSanctionRepository())
+            {
+                var loans = repo.GetAllExpiredLoans(4);
+                Assert.IsNotNull(loans);
+            }
+        }
+
+        [TestMethod]
+        public void IssueSanction()
+        {
+            var loan = new Loan()
+            {
+                book_name = "Test",
+                first_name = "testt",
+                last_name = "testt",
+                loan_status = "In progress",
+            };
+
+            using (var repo = new LoanHistoryAndSanctionRepository())
+            {
+                repo.IssueSanction(loan);
+                Assert.IsTrue(loan.loan_status == "In progress");
+            }
+        }
+    }
+}
