a5d0d637f20be824e32e3ec4c8e65ab31cd4642d
mustr <mustr@MARINO>
2024-01-29 01:22:28 +0100

    Testovi za SAP

--- Software/Scriptify/Unit_Tests/DataAcessLayerTest/Repositories/SAPRepositoryTest.cs
+++ Software/Scriptify/Unit_Tests/DataAcessLayerTest/Repositories/SAPRepositoryTest.cs
@@ -4,9 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DataAccessLayer;
 namespace Unit_Tests.DataAcessLayerTest.Repositories {
     [TestClass] 
     public class SAPRepositoryTest {
+        [TestMethod]
+        public void TestGetScriptifyAproovedData() {
+            // Arrange
+            SAPRepository sapRepository = new SAPRepository();
+            // Act
+            var result = sapRepository.getScriptifyAproovedData();
+            // Assert
+            Assert.IsNotNull(result);
+        }   
     }
 }
