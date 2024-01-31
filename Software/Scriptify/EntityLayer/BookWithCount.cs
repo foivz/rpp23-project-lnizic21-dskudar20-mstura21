d21406f592ce0a71608ae2c4fcd788d5bcc10df6
David Škudar <dskudar20@foi.hr>
2024-01-31 20:23:15 +0100

    Kreirana klasa za prikaz broja pojavljivanja neke knjige.

---
+++ Software/Scriptify/EntityLayer/BookWithCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class BookWithCount
+    {
+        public Book Book { get; set; }
+        public int LoanCount { get; set; }
+
+    }
+}
