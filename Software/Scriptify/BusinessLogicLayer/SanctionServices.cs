d56aaffad51e5863c9abc735d2cb3c727fafcfbd
David Škudar <dskudar20@foi.hr>
2024-02-06 23:09:13 +0100

    Implementiran SanctionServices.

---
+++ Software/Scriptify/BusinessLogicLayer/SanctionServices.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class SanctionServices
+    {
+
+        public bool SaveSanction(Sanction sanction)
+        {
+            bool isSuccessful = false;
+            using (var repo = new SanctionRepository())
+            {
+                int affectedRows = repo.SaveSanction(sanction);
+                isSuccessful = affectedRows > 0;
+            }
+            return isSuccessful;
+        }
+    }
+}
