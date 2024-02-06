b18ecffc7342a97d1ea429580e1b67809001930f
David Škudar <dskudar20@foi.hr>
2024-02-06 23:02:31 +0100

    Kreirana klasa "sanction".

---
+++ Software/Scriptify/EntityLayer/Sanction.cs
@@ -0,0 +1,36 @@
+﻿namespace EntityLayer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Sanction")]
+    public partial class Sanction
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int id_sanction { get; set; }
+
+        public int id_librarian { get; set; }
+        public int id_user { get; set; }
+
+
+        [StringLength(45)]
+        public string user_first_name { get; set; }
+
+        [StringLength(45)]
+        public string user_last_name { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? date_of_loan { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? planned_return { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? returned { get; set; }
+
+    }
+}
